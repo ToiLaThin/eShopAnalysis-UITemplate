aaadf07f02fb36c5a9e5dac892958eb303a2fb74
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-02-20 17:44:41 +0700

    restyle header 1, change banner

---
+++ src/core/layout/header/header-topbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'esa-header-top-bar',
+    templateUrl: './header-topbar.component.html'
+})
+export class HeaderTopbarComponent {
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,12 @@ module.exports = {
             transparent: 'transparent',
             current: 'currentColor',
             black: '#000',
+            primary: '#ff5252',
+            secondary: '#666',
+            tenery: '#ffffff',
             white: '#fff',
-            gray: {
-                50: '#f9fafb',
+            gray: {                
+                50: '#e5e5e5',
                 100: '#f4f5f7',
                 200: '#e5e7eb',
                 300: '#d2d6dc',
@@ -27,16 +30,60 @@ module.exports = {
             }
         },
         fontFamily: {
-            sans: ['Graphik', 'sans-serif'],
+            sans: ['Roboto', 'Arial', 'Helvetica', 'sans-serif'],
             serif: ['Merriweather', 'serif']
         },
+        fontWeight: {
+            thin: 100,
+            extralight: 200,
+            light: 300,
+            normal: 400,
+            medium: 500,
+            semibold: 600,
+            bold: 700,
+            extrabold: 800,
+            black: 900
+        },
+        letterSpacing: {
+            tighter: '-0.05em',
+            tight: '-0.025em',
+            normal: '0',
+            wide: '0.025em',
+            wider: '0.05em',
+            widest: '0.1em'
+        },
+        borderRadius: {
+            none: '0',
+            sm: '0.125rem',
+            DEFAULT: '5px',
+            md: '0.375rem',
+            lg: '0.5rem',
+            xl: '0.75rem',
+            '2xl': '1rem',
+            '3xl': '1.5rem',
+            full: '9999px'
+        },
         extend: {
-            spacing: {
+            spacing: {      
+                0: '0',
+                1: '0.25rem', // 4px
+                2: '0.5rem', // 8px
+                3: '0.75rem', // 12px
+                4: '1rem', // 16px
+                5: '1.25rem', // 20px
+                6: '1.5rem', // 24px
                 128: '32rem',
                 144: '36rem'
             },
             borderRadius: {
                 '4xl': '2rem'
+            },
+            fontSize: {
+                xs: ['0.75rem', { lineHeight: '1rem' }], // 12px
+                sm: ['0.875rem', { lineHeight: '1.25rem' }], // 14px
+                base: ['1rem', { lineHeight: '1.5rem' }], // 16px
+                lg: ['1.125rem', { lineHeight: '1.75rem' }], // 18px
+                xl: ['1.25rem', { lineHeight: '1.75rem' }], // 20px
             }
         }
     },
