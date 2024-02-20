bf9c18004eaf8d7ee936da549098ad00728f2d88
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-02-21 01:07:02 +0700

    + style header banner, footer
    + add more custom design token, component class
    + add feature, testimonial styling

--- src/core/layout/catalog-card.component.ts
+++ src/core/layout/catalog-card.component.ts
@@ -3,7 +3,6 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'esa-catalog-card',
     templateUrl: './catalog-card.component.html',
-    styleUrls: ['./catalog-card.component.css']
 
 })
 export class CatalogCardComponent {
