a4b24e6d191c9d494169b7fd72fc6947d44cbf6e
20110729 Nguyễn Đức Thịnh <20110729@student.hcmute.edu.vn>
2024-02-21 20:55:09 +0700

    + add index module, routes, router outlet
    + change components path

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,32 +1,25 @@
 import { NgModule } from '@angular/core';
-import { HeaderTopbarComponent } from './layout/header/header-topbar.component';
+import { HeaderTopbarComponent } from './layout/header-topbar.component';
 import { FooterComponent } from './layout/footer.component';
+import { HeaderBannerComponent } from './layout/header-banner.component';
+import { IndexModule } from '../index/index.module';
 import { CatalogCardComponent } from './layout/catalog-card.component';
-import { ProductCardComponent } from './layout/product-card.component';
-import { HeaderBannerComponent } from './layout/header/header-banner.component';
-import { FeatureCardComponent } from './layout/feature-card.component';
-import { TestimonialCardComponent } from './layout/testimonial-card.component';
 
 @NgModule({
     declarations: [
         HeaderTopbarComponent,
         HeaderBannerComponent,
-        FooterComponent,
-        CatalogCardComponent,
-        ProductCardComponent,
-        FeatureCardComponent,
-        TestimonialCardComponent
+        FooterComponent,    
+        CatalogCardComponent    
     ],
-    imports: [],
+    imports: [
+    ],
     providers: [],
     exports: [
         HeaderTopbarComponent,
         HeaderBannerComponent,
-        FooterComponent,
-        CatalogCardComponent,
-        ProductCardComponent,
-        FeatureCardComponent,
-        TestimonialCardComponent
+        FooterComponent,      
+        CatalogCardComponent  
     ]
 })
 export class CoreModule {}
